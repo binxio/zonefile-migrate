97a14691e7bc338939927b371f9542391c85166f
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2022-05-14 16:01:17 +0200

    formatting

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,7 @@ import unittest
 import doctest
 from aws_route53_migrate import zone_to_cfn
 
+
 def load_tests(loader, tests, pattern):
     suite = unittest.TestSuite()
     test = doctest.DocTestSuite(zone_to_cfn)
