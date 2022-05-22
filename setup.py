274aad637bbbeef216017008d1a7fc4a311033fe
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2022-05-22 11:22:28 +0200

    bumped  to release 0.3.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='zonefile-migrate',
-    version="0.2.1",
+    version="0.3.0",
     url='https://github.com/binxio/zonefile-migrate',
     license='BSD',
     author='Mark van Holsteijn',
